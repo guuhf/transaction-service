9d1b2927adbc64c00ae1b71a0f70077c994581a4
guuh <gustavo.jacintof@gmail.com>
2026-04-28 14:49:43 -0300

    feat: implement create transaction method and category entity

--- src/main/java/com/guuh/transaction_service/controller/TransactionController.java
+++ src/main/java/com/guuh/transaction_service/controller/TransactionController.java
@@ -1,4 +1,36 @@
 package com.guuh.transaction_service.controller;
 
+import com.guuh.transaction_service.business.TransactionService;
+import com.guuh.transaction_service.business.dto.request.TransactionRequestDto;
+import com.guuh.transaction_service.business.dto.response.TransactionResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+@Tag(name = "Transactions", description = "Transaction management")
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping
+    @Operation(summary = "Criar uma transação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação criada"),
+            @ApiResponse(responseCode = "409", description = "O valor da transação é inválido"),
+            @ApiResponse(responseCode = "500", description = "Erro inesperado")
+    })
+    private ResponseEntity<TransactionResponseDto> createTransaction(@RequestBody TransactionRequestDto dto){
+        return ResponseEntity.status(201).body(transactionService.createTransaction(dto));
+    }
+
 }
--- src/main/java/com/guuh/transaction_service/infrastructure/entity/Transaction.java
+++ src/main/java/com/guuh/transaction_service/infrastructure/entity/Transaction.java
@@ -1,5 +1,40 @@
 package com.guuh.transaction_service.infrastructure.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.guuh.transaction_service.infrastructure.enums.TransactionType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+@Builder
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "transaction_type")
+    private TransactionType transactionType;
+    @Column(name = "description", length = 150)
+    private String description;
+    @Column(name = "amount")
+    private BigDecimal amount;
+    @Column(name = "date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime date;
+    @Column(name = "due_date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
 
 }
