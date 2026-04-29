69565e29af535805ecbb403809a3504ca91cf919
guuh <gustavo.jacintof@gmail.com>
2026-04-29 17:37:29 -0300

    feat: implement create category method

--- src/main/java/com/guuh/transaction_service/business/dto/request/CategoryRequestDto.java
+++ src/main/java/com/guuh/transaction_service/business/dto/request/CategoryRequestDto.java
@@ -2,11 +2,13 @@ package com.guuh.transaction_service.business.dto.request;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class CategoryRequestDto {
-    private String category;
+    private String name;
 }
