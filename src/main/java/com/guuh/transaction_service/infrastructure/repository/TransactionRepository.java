06b7b02a3e0febc7dbd80e386870735224e6ee06
guuh <gustavo.jacintof@gmail.com>
2026-04-30 16:35:34 -0300

    metodo de pegar as transacoes

--- src/main/java/com/guuh/transaction_service/infrastructure/repository/TransactionRepository.java
+++ src/main/java/com/guuh/transaction_service/infrastructure/repository/TransactionRepository.java
@@ -4,5 +4,4 @@ import com.guuh.transaction_service.infrastructure.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
 }
