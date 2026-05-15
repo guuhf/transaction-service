3369de5382372cd4befebde266b9bc58a0f8d35c
guuh <gustavo.jacintof@gmail.com>
2026-05-15 16:16:28 -0300

    refactor: separate user features into AuthService and UserService

--- src/main/java/com/guuh/transaction_service/business/AuthService.java
+++ src/main/java/com/guuh/transaction_service/business/AuthService.java
@@ -7,14 +7,12 @@ import com.guuh.transaction_service.business.dto.response.UserResponseDto;
 import com.guuh.transaction_service.business.mapper.UserMapper;
 import com.guuh.transaction_service.infrastructure.entity.User;
 import com.guuh.transaction_service.infrastructure.exceptions.UserAlreadyExistsException;
-import com.guuh.transaction_service.infrastructure.exceptions.UserNotFoundException;
 import com.guuh.transaction_service.infrastructure.repository.UserRepository;
 import com.guuh.transaction_service.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -28,35 +26,25 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
-    public UserResponseDto userRegister(RegisterRequestDto dto){
+    public UserResponseDto userRegister(RegisterRequestDto dto) {
         validateEmailUniquess(dto.getEmail());
         User user = mapper.toUser(dto);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return mapper.toUserDto(userRepository.save(user));
     }
 
-    public void validateEmailUniquess(String email){
-        if (userRepository.existsByEmail(email)){
+    public void validateEmailUniquess(String email) {
+        if (userRepository.existsByEmail(email)) {
             throw new UserAlreadyExistsException("O usuário ja existe");
         }
     }
 
-    public LoginResponseDto userLogin(LoginRequestDto dto){
+    public LoginResponseDto userLogin(LoginRequestDto dto) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
         );
         return LoginResponseDto.builder()
-                .token("Bearer " + jwtUtil.generateToken(authentication.getName()) )
+                .token("Bearer " + jwtUtil.generateToken(authentication.getName()))
                 .build();
     }
-
-    public User getLoggedUser(){
-        String email = SecurityContextHolder
-                    .getContext()
-                    .getAuthentication()
-                    .getName();
-
-            return userRepository.findByEmail(email).orElseThrow(()->
-                    new UserNotFoundException("Usuário não encontrado"));
-        }
-    }
+}
--- src/main/java/com/guuh/transaction_service/business/TransactionService.java
+++ src/main/java/com/guuh/transaction_service/business/TransactionService.java
@@ -6,13 +6,11 @@ import com.guuh.transaction_service.business.dto.response.TransactionResponseDto
 import com.guuh.transaction_service.business.mapper.TransactionMapper;
 import com.guuh.transaction_service.infrastructure.entity.Category;
 import com.guuh.transaction_service.infrastructure.entity.Transaction;
-import com.guuh.transaction_service.infrastructure.entity.User;
 import com.guuh.transaction_service.infrastructure.exceptions.CategoryNotFoundException;
 import com.guuh.transaction_service.infrastructure.exceptions.InvalidAmountException;
 import com.guuh.transaction_service.infrastructure.repository.CategoryRepository;
 import com.guuh.transaction_service.infrastructure.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -25,14 +23,14 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final CategoryRepository categoryRepository;
     private final TransactionMapper mapper;
-    private final AuthService authService;
+    private final UserService userService;
 
     public TransactionResponseDto createTransaction(TransactionRequestDto dto) {
         amountValidation(dto.getAmount());
         Transaction transaction = mapper.toTransaction(dto);
         transaction.setDate(LocalDateTime.now());
-        transaction.setUser(authService.getLoggedUser());
-        Category category = categoryRepository.findByIdAndUserId(dto.getCategoryId(), authService.getLoggedUser().getId()).orElseThrow(() ->
+        transaction.setUser(userService.getLoggedUser());
+        Category category = categoryRepository.findByIdAndUserId(dto.getCategoryId(), userService.getLoggedUser().getId()).orElseThrow(() ->
                 new CategoryNotFoundException("Categoria não encontrada!"));
         transaction.getCategory().setName(category.getName());
         return mapper.toTransactionDto(transactionRepository.save(transaction));
@@ -46,7 +44,7 @@ public class TransactionService {
 
     public List<TransactionResponseDto> findTransactions(FilterRequestDto dto) {
         return mapper.toTransactionDtoList(transactionRepository.findWithFilter(
-                authService.getLoggedUser().getId(),
+                userService.getLoggedUser().getId(),
                 dto.getCategoryId(),
                 dto.getTransactionType(),
                 dto.getInitialDate(),
