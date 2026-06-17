c5c73bf0afab7caf4e9f902bb4aaccfa72a4f37c
guuh <gustavo.jacintof@gmail.com>
2026-06-17 10:59:17 -0300

    feat: implements a rate limiter in auth and report endpoints

--- src/main/java/com/guuh/transaction_service/infrastructure/handler/RestExceptionHandler.java
+++ src/main/java/com/guuh/transaction_service/infrastructure/handler/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.guuh.transaction_service.infrastructure.handler;
 
 import com.guuh.transaction_service.infrastructure.exceptions.*;
-import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -38,8 +37,8 @@ public class RestExceptionHandler {
 
     @ExceptionHandler(BadCredentialsException.class)
     private ResponseEntity<RestErrorMessage> badCredentialsHandler(BadCredentialsException e) {
-        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.UNAUTHORIZED, "Email ou senha inválidos");
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(threatResponse);
+        RestErrorMessage threatResponse = new RestErrorMessage(HttpStatus.CONFLICT, "Email ou senha inválidos");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(threatResponse);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
