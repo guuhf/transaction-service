6d702157a616d243ef0c1c01c1310a9e005089db
guuh <gustavo.jacintof@gmail.com>
2026-07-21 16:48:22 -0300

    refactor: implements a constant for Bearer and for token_typer and removed unused imports

--- src/main/java/com/guuh/transaction_service/controller/ReportController.java
+++ src/main/java/com/guuh/transaction_service/controller/ReportController.java
@@ -3,7 +3,6 @@ package com.guuh.transaction_service.controller;
 import com.guuh.transaction_service.business.ReportService;
 import com.guuh.transaction_service.business.dto.response.ReportResponseDto;
 import com.guuh.transaction_service.infrastructure.configs.AuthorizationConfig;
-import com.guuh.transaction_service.infrastructure.security.SecurityConfig;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,7 +10,10 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
@@ -30,7 +32,7 @@ public class ReportController {
             @ApiResponse(responseCode = "200", description = "Relatório criado"),
             @ApiResponse(responseCode = "500", description = "Erro inesperado")
     })
-    private ResponseEntity<ReportResponseDto> generateReport(@RequestParam("initialDate")LocalDateTime initialDate,
+    public ResponseEntity<ReportResponseDto> generateReport(@RequestParam("initialDate")LocalDateTime initialDate,
                                           @RequestParam("finalDate")LocalDateTime finalDate){
         return ResponseEntity.status(200).body(reportService.generateReport(initialDate,finalDate));
     }
